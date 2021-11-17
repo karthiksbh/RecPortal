3de659b4b5f54edf95c76ac1941760199b71d84e
karthik <karthiksbh@gmail.com>
2021-11-17 22:46:08 +0530

    commit host change

--- recportal/settings.py
+++ recportal/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = "django-insecure-qx!zy9eo93!yq+(ye+&8y^u5^rz0d4t48^gko7u1m!4fx^l)0-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['recportal-iete.herokuapp.com', '127.0.0.1']
+# ALLOWED_HOSTS = ['recportal-iete.herokuapp.com', '127.0.0.1']
+
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'app.User'
 
