bc23a208c102dcce4d28f2809801b5679f0df34f
karthik <karthiksbh@gmail.com>
2021-11-17 19:28:55 +0530

    New commit dep

--- recportal/settings.py
+++ recportal/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 AUTH_USER_MODEL = 'app.User'
 
